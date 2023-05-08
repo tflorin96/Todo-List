181d09bb776eeccb451cfe3e92c8dfa83b8249b1
tflorin96 <132939381+tflorin96@users.noreply.github.com>
2023-05-09 02:34:16 +0300

    Update App.js

--- nodejs/src/App.js
+++ nodejs/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello World!</p>
+        <p>Hello World! hahaha</p>
       </header>
     </div>
   );
