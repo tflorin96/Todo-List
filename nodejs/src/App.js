46d49e090a0e7c2d4b480856303b9ee3aa94e034
Florin Trifan <trifan.florin96@yahoo.com>
2023-05-09 18:05:49 +0300

    add the TodoList changes

--- nodejs/src/App.js
+++ nodejs/src/App.js
@@ -1,9 +1,9 @@
-import Header from './components/Header/Header';
+import TodoList from './components/TodoList/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <TodoList />
     </div>
   );
 }
---
+++ nodejs/src/components/TodoList/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './popup.module.css';
+
+
+function TodoList() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default TodoList;
