bd96bf376a9cc77fa4d3f90b868f3c6c734c2908
Florin Trifan <trifan.florin96@yahoo.com>
2023-05-09 03:18:13 +0300

    added header, main and footer components

--- nodejs/src/App.js
+++ nodejs/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello World! hahaha</p>
-      </header>
+      <Header />
     </div>
   );
 }
