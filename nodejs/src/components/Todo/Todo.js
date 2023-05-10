be443b270e94622ede879de628ca377c0b913488
Florin Trifan <trifan.florin96@yahoo.com>
2023-05-10 18:42:50 +0300

    Added checkbox for each task, some css adjustments

--- nodejs/src/components/Todo/Todo.js
+++ nodejs/src/components/Todo/Todo.js
@@ -6,8 +6,10 @@ function Todo({task, removeTask}) {
   function handleRemoveTask() {
     removeTask(task.id);
   }
+
   return (
     <div className={styles.todo_container}>
+      <input className={styles.mark_complete} type='checkbox' />
       <p className={styles.task_name}>{task.name}</p>
       <img className={styles.bin_icon} src="./binIcon.png" alt='Bin icon' onClick={handleRemoveTask}/>
     </div>
